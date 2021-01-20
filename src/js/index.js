d5b53a6d12d6e37998c1408e9da8c0a897da7336
PauloMarxs <paulomarquestelles@gmail.com>
2021-01-20 14:24:36 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 console.log("teste");
+
+/*criação de váriavel para observar a mudança de media queries*/
 let mql = window.matchMedia("(max-width: 639px)");
 
 let toogleMenuButton = document.querySelector(".menu__toogle");
 let menuToogle = document.querySelector(".menu__mainmenu");
 
+/*adicionei um listener para identificar a mudança de @media e esconder o menu caso aconteça*/
 mql.addEventListener("change", () => {
   if (menuToogle.classList.contains("menuactive")) {
     return menuToogle.classList.remove("menuactive");
