fd91cbad4bd853a801001b2f77af2aef3f3a7b75
PauloMarxs <paulomarquestelles@gmail.com>
2021-01-21 12:09:52 -0300

    Correções
    Alteração com as correções especificadas e implementação da animação do menu que estava faltando.

--- src/js/index.js
+++ src/js/index.js
@@ -5,18 +5,22 @@ let mql = window.matchMedia("(max-width: 639px)");
 
 let toogleMenuButton = document.querySelector(".menu__toogle");
 let menuToogle = document.querySelector(".menu__mainmenu");
+let menuIconToogle = document.querySelector(".menu__toogle");
 
-/*adicionei um listener para identificar a mudança de @media e esconder o menu caso aconteça*/
+/*adicionei um listener para identificar a mudança de @media e esconder o menu.*/
 mql.addEventListener("change", () => {
   if (menuToogle.classList.contains("menuactive")) {
+    menuIconToogle.classList.remove("active");
     return menuToogle.classList.remove("menuactive");
   }
 });
 
 toogleMenuButton.addEventListener("click", () => {
   if (menuToogle.classList.contains("menuactive")) {
+    menuIconToogle.classList.remove("active");
     return menuToogle.classList.remove("menuactive");
   }
 
+  menuIconToogle.classList.add("active");
   return menuToogle.classList.add("menuactive");
 });
