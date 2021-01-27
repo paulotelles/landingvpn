c9a433915ca53542a60ee1aa1fae42ef9e773464
PauloMarxs <paulomarquestelles@gmail.com>
2021-01-27 16:55:03 -0300

    alterações em 27/01
    seguem altearções

--- src/js/index.js
+++ src/js/index.js
@@ -69,3 +69,30 @@ var swiperCard = new Swiper(".swiper-card", {
     },
   },
 });
+
+var swiperCard = new Swiper(".swiper-card2", {
+  initialSlide: 1,
+  spaceBetween: 31,
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".button-forward",
+    prevEl: ".button-backward",
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: "auto",
+      centeredSlides: true,
+      initialSlide: 1,
+    },
+  },
+});
